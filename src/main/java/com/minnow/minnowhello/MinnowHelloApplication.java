abb08263e5bc70bc1382d550f30e8e10535e649c
minnow <2491562126@qq.com>
2023-03-28 23:17:33 +0800

    project initialization

---
+++ src/main/java/com/minnow/minnowhello/MinnowHelloApplication.java
@@ -0,0 +1,13 @@
+package com.minnow.minnowhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinnowHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinnowHelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minnow/minnowhello/MinnowHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minnow.minnowhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinnowHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
